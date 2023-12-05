fde2ebcf7d7c2e1849b5ec6bc075ba75b1fd3e7a
Nagendra <nagendrababu.marasu@gmail.com>
2023-12-05 05:51:01 +0530

    merge sorted arrays/files using min-heap

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,28 @@
+import heapq
 from typing import List
 
 from test_framework import generic_test
 
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    min_heap = []
+    sorted_arrays_iters = [iter(x) for x in sorted_arrays]
+
+    for i, it in enumerate(sorted_arrays_iters):
+        next_element = next(it, None)
+        if next_element is not None:
+            heapq.heappush(min_heap, (next_element, i))
+
+    result = []
+    while min_heap:
+        smallest_entry, smallest_entry_i = heapq.heappop(min_heap)
+        smallest_entry_iter = sorted_arrays_iters[smallest_entry_i]
+        result.append(smallest_entry)
+        next_element = next(smallest_entry_iter, None)
+        if next_element is not None:
+            heapq.heappush(min_heap, (next_element , smallest_entry_i))
+
+    return result
 
 
 if __name__ == '__main__':

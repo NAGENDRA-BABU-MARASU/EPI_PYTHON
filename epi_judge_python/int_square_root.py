afc3be7b38915bb55a4fa91f95eb8e90ee1eadfd
Nagendra <nagendrababu.marasu@gmail.com>
2023-12-09 06:55:43 +0530

    sorted matrix search

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    left = 0
+    right = k
+
+    while left <= right:
+        middle = (left + right) // 2
+        if middle**2 > k:
+            right = middle - 1
+        else:
+            left = middle + 1
+    return left - 1
 
 
 if __name__ == '__main__':
--- epi_judge_python/search_entry_equal_to_index.py
+++ epi_judge_python/search_entry_equal_to_index.py
@@ -7,8 +7,19 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def search_entry_equal_to_its_index(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    left = 0
+    right = len(A) -1
+    while left <= right:
+        mid = left + (right - left) // 2
+        difference = A[mid] - mid
+        if difference == 0:
+            return mid
+        elif difference > 0:
+            right = mid -1
+        else:
+            left = mid + 1
+    return -1
+
 
 
 @enable_executor_hook

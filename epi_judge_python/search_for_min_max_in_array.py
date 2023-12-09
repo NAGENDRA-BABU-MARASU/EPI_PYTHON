ae3e6e170e2c55a474080c52e6041c0bba424736
Nagendra <nagendrababu.marasu@gmail.com>
2023-12-09 07:31:40 +0530

    find min max at a time

--- epi_judge_python/search_for_min_max_in_array.py
+++ epi_judge_python/search_for_min_max_in_array.py
@@ -4,12 +4,34 @@ from typing import List
 from test_framework import generic_test
 from test_framework.test_failure import PropertyName
 
-MinMax = collections.namedtuple('MinMax', ('smallest', 'largest'))
+# MinMax = collections.namedtuple('MinMax', ('smallest', 'largest'))
 
 
-def find_min_max(A: List[int]) -> MinMax:
+def find_min_max(A: List[int]):
     # TODO - you fill in here.
-    return MinMax(0, 0)
+    def min_max(a,b):
+        return (a,b) if a < b else (b,a)
+    if len(A) <= 1:
+        return (A[0], A[0])
+
+    global_min_max = min_max(A[0], A[1])
+    for i in range(2, len(A) - 1, 2):
+        local_min_max = min_max(A[i], A[i+1])
+        global_min_max = (
+            min(global_min_max[0], local_min_max[0]),
+            max(global_min_max[1], local_min_max[1])
+        )
+
+
+    if len(A) % 2 != 0:
+        global_min_max = (
+            min(global_min_max[0], A[-1]),
+            max(global_min_max[1], A[-1])
+        )
+
+    return global_min_max
+
+
 
 
 def res_printer(prop, value):
